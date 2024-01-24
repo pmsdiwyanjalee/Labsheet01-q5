84a51ac69340a48bfdb2e0431c93dd85375a531d
User <User@DESKTOP-2OOA2TK>
2024-01-24 16:59:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Labsheet05
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+
+        string userInput = Console.ReadLine();
+
+        if (int.TryParse(userInput, out int number))
+        {
+            DisplayMultiplicationTable(number);
+        }
+        else
+        {
+            Console.WriteLine("ERROR: Please enter a valid integer.");
+        }
+    }
+
+    static void DisplayMultiplicationTable(int number)
+    {
+        Console.WriteLine($"Multiplication Table for {number}:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int result = number * i;
+            Console.WriteLine($"{number} x {i} = {result}");
+            Console.ReadLine();
+        }
+    }
+}
+
